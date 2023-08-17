6b99a7eb63ef8b0a8ac0ca30fef53535509ef015
oteffahi <oteffahi@yahoo.com>
2023-08-17 19:11:50 +0000

    Renamed directory cmd to filebankd

--- cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import cmd "github.com/oteffahi/merkle-filebank/cmd/commands"
-
-func main() {
-	cmd.Execute()
-}
---
+++ filebankd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import cmd "github.com/oteffahi/merkle-filebank/filebankd/commands"
+
+func main() {
+	cmd.Execute()
+}
